bfa7851fd827a2cf6e5853e6b4ac7b5d442ac41b
Ruben Kuipers <ruben@rubenkuipers.dev>
2022-01-18 10:24:55 +0100

    Bump client, android & ios SDK versions

--- scripts/downloadiOSSDK.js
+++ scripts/downloadiOSSDK.js
@@ -1,31 +1,34 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var IosSDKVersion = "OpenTok-iOS-2.16.6";
+    var IosSDKVersion = 'OpenTok-iOS-2.21.3'
     var downloadFile = require('./downloadFile.js'),
         exec = require('./exec/exec.js'),
         Q = require('q'),
-        deferral = new Q.defer();
-    console.log('Downloading OpenTok iOS SDK');
-    downloadFile('https://s3.amazonaws.com/artifact.tokbox.com/rel/ios-sdk/' + IosSDKVersion + '.tar.bz2',
-        './' + IosSDKVersion + '.tar.bz2', function (err) {
+        deferral = new Q.defer()
+    console.log('Downloading OpenTok iOS SDK')
+    downloadFile(
+        'https://s3.amazonaws.com/artifact.tokbox.com/rel/ios-sdk/' + IosSDKVersion + '.tar.bz2',
+        './' + IosSDKVersion + '.tar.bz2',
+        function (err) {
             if (!err) {
-                console.log('downloaded');
+                console.log('downloaded')
                 exec('tar -zxvf ./' + IosSDKVersion + '.tar.bz2', function (err, out, code) {
-                    console.log('expanded');
-                    var frameworkDir = context.opts.plugin.dir + '/src/ios/';
+                    console.log('expanded')
+                    var frameworkDir = context.opts.plugin.dir + '/src/ios/'
                     exec('mv ./' + IosSDKVersion + '/OpenTok.framework ' + frameworkDir, function (err, out, code) {
-                        console.log('moved OpenTok.framework into ' + frameworkDir);
+                        console.log('moved OpenTok.framework into ' + frameworkDir)
                         exec('rm -r ./' + IosSDKVersion, function (err, out, code) {
-                            console.log('Removed extracted dir');
+                            console.log('Removed extracted dir')
                             exec('rm ./' + IosSDKVersion + '.tar.bz2', function (err, out, code) {
-                                console.log('Removed downloaded SDK');
-                                deferral.resolve();
-                            });
-                        });
-                    });
-                });
+                                console.log('Removed downloaded SDK')
+                                deferral.resolve()
+                            })
+                        })
+                    })
+                })
             }
-        });
-    return deferral.promise;
-};
+        }
+    )
+    return deferral.promise
+}
